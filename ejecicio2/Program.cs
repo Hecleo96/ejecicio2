a5cc165a71c8fe478f8bb83ef83977856b758200
HECTOR LEONEL RUIZ TREJO <ISNP645615@ugb.edu.sv>
2024-04-20 10:42:03 -0600

    Agregar archivos de proyecto.

---
+++ ejecicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejecicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Desarolle un programa que escriba en pantalla los números del 1 al 10, usando "While"
+            int numero = 1;
+
+            Console.WriteLine("Números del 1 al 10: ");
+            while (numero <= 10)
+            {
+                Console.WriteLine(numero);
+                numero++;
+            }
+            Console.WriteLine("Presione Enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
